907e9581d3209276922963e4262a87ecbd54a031
saganeupane <sagar.neupane@varicon.com.au>
2024-10-30 11:35:53 +0545

    Exposing Django to local network

--- backend/settings.py
+++ backend/settings.py
@@ -142,7 +142,7 @@ CORS_ALLOW_HEADERS = [
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(hours=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
